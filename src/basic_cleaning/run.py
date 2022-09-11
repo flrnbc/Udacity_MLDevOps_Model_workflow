0fd833f8bbe7243359e68c737ce79bfd0e526a88
Florian Beck <flrn.beck@gmail.com>
2022-09-11 11:55:20 +0200

    Geolocation restriction

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     logger.info("Data cleaning: revert last_review to datetime.")
     df["last_review"] = pd.to_datetime(df["last_review"])
 
+    logger.info("Impose geolocation restriction.")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Save and log output artifact.")
     # export to csv
     df.to_csv("clean_sample.csv", index=False) # TODO: index?
